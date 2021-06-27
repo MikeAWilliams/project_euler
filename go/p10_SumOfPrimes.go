cdd12c0fe1d56fa5a7de234f05547f7d6075558e
Mike Williams <mumakin@gmail.com>
2021-06-26 22:14:58 -0600

    convert p10 to use sieve

--- go/p10_SumOfPrimes.go
+++ go/p10_SumOfPrimes.go
@@ -2,34 +2,22 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"maw/util"
 )
 
-func isPrime(num int) bool {
-	maxf := math.Sqrt(float64(num))
-	max := int(maxf)
-	if num%2 == 0 {
-		return false
-	}
-	for i := 3; i <= max; i += 2 {
-		if num%i == 0 {
-			return false
-		}
-	}
-	return true
-}
-
 func sumPrimesBelow(max int) int {
-	result := 2
-	for candidate := 3; candidate < max; candidate += 2 {
-		if isPrime(candidate) {
-			result += candidate
+	flags := util.GetPrimesBelowFlags(max)
+	result := 0
+	for index, flag := range flags {
+		if flag {
+			result += index + 2
 		}
 	}
 	return result
 }
 
 func main() {
-	fmt.Println(sumPrimesBelow(10))
-	fmt.Println(sumPrimesBelow(2000000))
+	result := (sumPrimesBelow(2000000))
+	delta := 142913828922 - result
+	fmt.Println(delta)
 }
