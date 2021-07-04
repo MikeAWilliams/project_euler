08ad807ab65f347cb2ea208cac5e86be1f88d214
Mike Williams <mumakin@gmail.com>
2021-07-03 23:17:45 -0600

    more tests

--- go/util/bigSum_test.go
+++ go/util/bigSum_test.go
@@ -10,4 +10,5 @@ func TestBigSum(t *testing.T) {
 	requireEqualSlices(t, []int{1, 7}, util.SumLargeInt([][]int{{9}, {8}}))
 	requireEqualSlices(t, []int{3, 7, 5}, util.SumLargeInt([][]int{{1, 9, 9}, {1, 7, 6}}))
 	requireEqualSlices(t, []int{4, 2, 5}, util.SumLargeInt([][]int{{1, 9, 9}, {5, 0}, {1, 7, 6}}))
+	requireEqualSlices(t, []int{1, 2, 3, 4}, util.SumLargeInt([][]int{{1, 1, 1, 1}, {1, 1, 1}, {1, 1}, {1}}))
 }
