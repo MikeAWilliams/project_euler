8bf2c2bfba8f1e75af3a63189595f70e31b66ff5
Mike Williams <mumakin@gmail.com>
2021-07-04 12:04:33 -0600

    cheated

--- go/p15_LaticePath.go
+++ go/p15_LaticePath.go
@@ -14,8 +14,12 @@ func (c coordinate) moveDown() coordinate {
 	return coordinate{x: c.x, y: c.y + 1}
 }
 
+func (c coordinate) equal(other coordinate) bool {
+	return c.x == other.x && c.y == other.y
+}
+
 func countMovesFromPosition(current coordinate, max coordinate, accumulator int) int {
-	if max.x == current.x && max.y == current.y {
+	if max.equal(current) {
 		return accumulator + 1
 	}
 	if current.x < max.x {
@@ -27,7 +31,55 @@ func countMovesFromPosition(current coordinate, max coordinate, accumulator int)
 	return accumulator
 }
 
-func main() {
-	fmt.Println(countMovesFromPosition(coordinate{x: 0, y: 0}, coordinate{x: 2, y: 2}, 0))
+func countMoves(max coordinate) int {
+	result := 0
+	current := coordinate{x: 0, y: 0}
+	pivots := []coordinate{current}
+	for len(pivots) > 0 {
+		current, pivots = pivots[len(pivots)-1], pivots[0:len(pivots)-1]
+		if max.x > current.x {
+			next := current.moveRight()
+			if max.equal(next) {
+				result += 1
+			} else {
+				pivots = append(pivots, next)
+			}
+		}
+		if max.y > current.y {
+			next := current.moveDown()
+			if max.equal(next) {
+				result += 1
+			} else {
+				pivots = append(pivots, next)
+			}
+		}
+	}
 
+	return result
+}
+
+func countMovesFromEnd(size int) int64 {
+	grid := [][]int64{}
+	for j := 0; j <= size; j++ {
+		grid = append(grid, make([]int64, size+1))
+	}
+
+	for i := 0; i < size; i++ {
+		grid[i][size] = 1
+		grid[size][i] = 1
+	}
+	for i := size - 1; i >= 0; i-- {
+		for j := size - 1; j >= 0; j-- {
+			grid[i][j] = grid[i+1][j] + grid[i][j+1]
+		}
+	}
+	return grid[0][0]
+}
+
+func main() {
+	fmt.Println(countMoves(coordinate{x: 2, y: 2}))
+	fmt.Println(countMovesFromEnd(2))
+	fmt.Println(countMoves(coordinate{x: 10, y: 10}))
+	fmt.Println(countMovesFromEnd(10))
+	fmt.Println(countMovesFromEnd(20))
 }
