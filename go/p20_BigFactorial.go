e9d9e8f12c7f144b3f0b4e9ec83593772177d871
Mike Williams <mumakin@gmail.com>
2021-07-05 14:37:53 -0600

    p20

--- go/p20_BigFactorial.go
+++ go/p20_BigFactorial.go
@@ -3,18 +3,23 @@ package main
 import (
 	"fmt"
 	"math/big"
+	"maw/util"
 )
 
-func bigFactorial(n int) big.Int {
-
+func bigFactorial(n int64) *big.Int {
+	result := big.NewInt(n)
+	for n--; n > 0; n-- {
+		result = result.Mul(result, big.NewInt(n))
+	}
+	return result
 }
 
-func sumDigitsBigFactorial(n int) int {
+func sumDigitsBigFactorial(n int64) int64 {
 	bigNumber := bigFactorial(n)
-
-	return 1
+	return util.SumDigitsBigInt(bigNumber)
 }
 
 func main() {
-	fmt.Println(sumDigitsBigFactorial(10))
+	fmt.Println(sumDigitsBigFactorial(int64(10)))
+	fmt.Println(sumDigitsBigFactorial(int64(100)))
 }
