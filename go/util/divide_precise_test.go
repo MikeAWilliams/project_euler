e08072c16c59b9eff86c2bca608f922641747c0d
Mike Williams <mumakin@gmail.com>
2021-07-25 09:01:04 -0600

    progress

--- go/util/divide_precise_test.go
+++ go/util/divide_precise_test.go
@@ -1,15 +1,21 @@
 package util_test
 
 import (
+	"fmt"
 	"maw/util"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
+func requireEqualDivisionResult(t *testing.T, expected, recieved util.DivisionResult) {
+	failMessage := fmt.Sprintf("expected %+v, recieved %+v", expected, recieved)
+	require.Equal(t, expected.Whole, recieved.Whole, failMessage)
+	require.Equal(t, expected.Termination, recieved.Termination, failMessage)
+	requireEqualSlicesU8(t, expected.FractionDigits, recieved.FractionDigits)
+}
+
 func TestDividePrecise(t *testing.T) {
 	tenth := util.Divide(1, 10)
-	require.Equal(t, uint64(0), tenth.Whole)
-	require.Equal(t, util.Terminates, tenth.Termination)
-	requireEqualSlicesU8(t, []uint8{1}, tenth.FractionDigits)
+	requireEqualDivisionResult(t, util.DivisionResult{Whole: 0, FractionDigits: []uint8{1}, Termination: util.Terminates}, tenth)
 }
