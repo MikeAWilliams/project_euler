ba5af218f6c8af989c0ec7754fe3c555390cf7fe
Mike Williams <mumakin@gmail.com>
2021-07-24 23:52:36 -0600

    progress on 26

--- go/p26_cycles.go
+++ go/p26_cycles.go
@@ -1,10 +1,22 @@
 package main
-import "fmt"
 
-func main() {
-    for d:=0.0; d < 1000.0; d++{
-        result := 1.0/d
-        fmt.Printf("%v, %v\n",d, result)
+import (
+	"fmt"
+	"math/big"
+)
+
+func getQuotients(max int, digits int)[]string{
+    quotients := make([]string, 1000)
+    one:= big.NewFloat(1.0)
+    for d:= 0; d < 1000; d++ {
+        bigD := big.NewFloat(float64(d))
+        quotient := big.NewFloat(1.0).Quo(one, bigD)
+        textQuotient := quotient.Text('f', 40)
+        quotients[d]=textQuotient
     }
-
+    return quotients
+}
+func main() {
+    quotients := getQuotients(1000, 40)
+    fmt.Println(quotients)
 }
