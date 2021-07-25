7c33141f2fc82c654aa618f05647c57a58af2f9f
Mike Williams <mumakin@gmail.com>
2021-07-25 08:54:18 -0600

    simple works

--- go/util/divide_precise.go
+++ go/util/divide_precise.go
@@ -15,5 +15,16 @@ type DivisionResult struct {
 }
 
 func Divide(num int, denom int) DivisionResult {
-	return DivisionResult{}
+	var result DivisionResult
+	uNum := uint64(num)
+	uDenom := uint64(denom)
+	result.Whole = uNum / uDenom
+	remainder := uNum % uDenom
+	for 0 != remainder {
+		newNum := remainder * 10
+		digit := newNum / uDenom
+		remainder = newNum % uDenom
+		result.FractionDigits = append(result.FractionDigits, uint8(digit))
+	}
+	return result
 }
