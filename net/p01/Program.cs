372576ccf06545c90ce317869b80453ef2db5c32
Mike Williams <mumakin@gmail.com>
2026-06-15 20:26:14 -0600

    p01

--- net/p01/Program.cs
+++ net/p01/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+static void SayHello(string name)
+{
+    Console.WriteLine($"Hello, {name}!");
+}
+
+static bool IsDivisibleBy(int num, int den)
+{
+    return num % den == 0;
+}
+
+static int SumOfMultiplesThreeAndFive(int maxNum)
+{
+    int result = 0;
+    for (int i = 0; i < maxNum; i++)
+    {
+        if (IsDivisibleBy(i, 3) || IsDivisibleBy(i, 5))
+        {
+            result += i;
+        }
+    }
+    return result;
+}
+
+Console.WriteLine(SumOfMultiplesThreeAndFive(1000));
