0ad9959016afe3f51da17d6c829dfd91743e064a
Mike Williams <mumakin@gmail.com>
2026-06-19 12:15:47 -0600

    09

---
+++ net/p09/Program.cs
@@ -0,0 +1,14 @@
+﻿for (int a = 1; a < 1000; a++)
+{
+    for (int b = a + 1; b < 1000; b++)
+    {
+        int c = 1000 - a - b;
+        if (c < b || c < a) break;
+        if (a * a + b * b == c * c)
+        {
+            Console.WriteLine($"a={a}, b={b}, c={c}");
+            Console.WriteLine($"a*a={a * a}, b*b={b * b}, c*c={c * c}");
+            Console.WriteLine($"a*b*c={a * b * c}");
+        }
+    }
+}
