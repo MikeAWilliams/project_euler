9ffa84a84cd5d068be53e0e41c1e09742f782976
Mike Williams <mumakin@gmail.com>
2026-06-19 10:20:08 -0600

    5

---
+++ net/p05/Program.cs
@@ -0,0 +1,28 @@
+﻿static bool TestForRemainderInRange(long num, int max)
+{
+    for (int div = 2; div <= max; div++)
+    {
+        if (num % div > 0)
+        {
+            return false;
+        }
+
+    }
+    return true;
+}
+
+static long FindSmallestNoRemainder(int maxDivisor, long maxCandidate)
+{
+    for (long candidate = maxDivisor; candidate < maxCandidate; candidate++)
+    {
+        if (TestForRemainderInRange(candidate, maxDivisor))
+        {
+            return candidate;
+        }
+    }
+    return 0;
+}
+
+Console.WriteLine(TestForRemainderInRange(2520, 10));
+Console.WriteLine(FindSmallestNoRemainder(10, 10000000000));
+Console.WriteLine(FindSmallestNoRemainder(20, 10000000000));
