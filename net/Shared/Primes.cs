0195b354905f655893fcfda1c5117c35724702b4
Mike Williams <mumakin@gmail.com>
2026-06-19 11:03:37 -0600

    sieve is working

--- net/Shared/Primes.cs
+++ net/Shared/Primes.cs
@@ -1,9 +1,54 @@
 namespace Shared;
 
-public class Stupid
+// Sieve of Eratosthenes
+public class Sieve
 {
-    public static int Test()
+
+    // if _flags[i] is true then i is prime
+    private readonly bool[] _flags;
+    private readonly int _count;
+    public Sieve(int count)
     {
-        return 42;
+        _count = count;
+        // defaults false
+        _flags = new bool[count];
+        InitFlags();
+        RunSieve();
+    }
+
+    public bool IsPrime(int num)
+    {
+        if (num > _count)
+        {
+            throw new IndexOutOfRangeException();
+        }
+        return _flags[num];
+    }
+
+    private void InitFlags()
+    {
+        _flags[2] = true;
+        // all odd numbers might be prime
+        for (int i = 3; i < _count; i += 2)
+        {
+            _flags[i] = true;
+        }
+    }
+
+    private void RunSieve()
+    {
+        int candidate = 3;
+        // search upto sqrt(count)
+        while (candidate * candidate < _count)
+        {
+            // starting with 9 (3*3) because evens already false, odds before 9 are prime
+            // advance by candidate*2
+            for (int notPrime = candidate * candidate; notPrime < _count; notPrime += candidate * 2)
+            {
+                _flags[notPrime] = false;
+            }
+            // find the next candidate
+            for (candidate += 2; !_flags[candidate]; candidate += 2) { }
+        }
     }
 }
--- net/p07/Program.cs
+++ net/p07/Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine(Shared.Stupid.Test());
+﻿Shared.Sieve sieve = new Shared.Sieve(100);
+
+for (int i = 0; i < 30; i++)
+{
+    Console.WriteLine($"{i} is prime {sieve.IsPrime(i)}");
+}
