80b910031e73a7d52c28596c4b3bdad3ea1fa2e6
Mike Williams <mumakin@gmail.com>
2026-06-19 12:24:47 -0600

    p10

--- net/p10/Program.cs
+++ net/p10/Program.cs
@@ -1,8 +1,8 @@
 ﻿const int size = 2000000;
 var sieve = new Shared.Sieve(size);
 ulong sum = 0;
-for (uint i = 2; i < size; i++)
+for (int i = 2; i < size; i++)
 {
-    if (sieve.IsPrime((int)(i))) sum += i;
+    if (sieve.IsPrime(i)) sum += (uint)i;
 }
 Console.WriteLine(sum);
