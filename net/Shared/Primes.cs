ecf3bf8ac382f84b3ca94821468eba09efcfddfb
Mike Williams <mumakin@gmail.com>
2026-06-19 11:11:27 -0600

    cleanup

--- net/Shared/Primes.cs
+++ net/Shared/Primes.cs
@@ -6,19 +6,19 @@ public class Sieve
 
     // if _flags[i] is true then i is prime
     private readonly bool[] _flags;
-    private readonly int _count;
-    public Sieve(int count)
+    private readonly int _size;
+    public Sieve(int size)
     {
-        _count = count;
+        _size = size;
         // defaults false
-        _flags = new bool[count];
+        _flags = new bool[size];
         InitFlags();
         RunSieve();
     }
 
     public bool IsPrime(int num)
     {
-        if (num > _count)
+        if (num < 0 || num >= _size)
         {
             throw new IndexOutOfRangeException();
         }
@@ -29,7 +29,7 @@ public class Sieve
     {
         _flags[2] = true;
         // all odd numbers might be prime
-        for (int i = 3; i < _count; i += 2)
+        for (int i = 3; i < _size; i += 2)
         {
             _flags[i] = true;
         }
@@ -38,18 +38,19 @@ public class Sieve
     private void RunSieve()
     {
         int candidate = 3;
-        // search upto sqrt(count)
-        while (candidate * candidate < _count)
+        // search upto sqrt(size)
+        while (candidate * candidate < _size)
         {
             // starting with 9 (3*3) because evens already false, odds before 9 are prime
             // advance by candidate*2 to skip even multiples of candidate (already false)
-            for (int notPrime = candidate * candidate; notPrime < _count; notPrime += candidate * 2)
+            int advance = candidate * 2;
+            for (int notPrime = candidate * candidate; notPrime < _size; notPrime += advance)
             {
                 _flags[notPrime] = false;
             }
             // find the next candidate
             // next item not already crossed out
-            for (candidate += 2; !_flags[candidate]; candidate += 2) { }
+            for (candidate += 2; candidate < _size && !_flags[candidate]; candidate += 2) { }
         }
     }
 }
