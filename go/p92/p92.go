3f6fda5604fae1c43c21103e67dca08350708ee9
Mike Williams <mumakin@gmail.com>
2026-06-20 14:19:33 -0600

    p92 in go

---
+++ go/p92/p92.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func getNext(n int) int {
+	sum := 0
+	for n > 0 {
+		d := n % 10
+		sum += d * d
+		n = n / 10
+	}
+	return sum
+}
+
+func ends89(n int) bool {
+	for n != 1 && n != 89 {
+		n = getNext(n)
+	}
+	return n == 89
+}
+
+func main() {
+	count := 0
+	for i := 1; i < 10000000; i++ {
+		if ends89(i) {
+			count += 1
+		}
+	}
+	fmt.Println(count)
+}
