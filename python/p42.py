41bccc9e66d072d5a8573067d60025a76d10a3d6
Mike Williams <mumakin@gmail.com>
2026-06-21 17:05:52 -0600

    use set

--- python/p42.py
+++ python/p42.py
@@ -1,14 +1,7 @@
 import csv
 import string
 from bisect import bisect_left
-
-
-def binary_search(sorted_list, target):
-    index = bisect_left(sorted_list, target)
-
-    if index < len(sorted_list) and sorted_list[index] == target:
-        return index
-    return -1
+from termios import tcdrain
 
 
 def get_data():
@@ -34,11 +27,11 @@ def get_triangle(n):
 
 
 def get_triange_numbers(max):
-    result = []
+    result = set()
     i = 1
     t = get_triangle(i)
     while t <= max:
-        result.append(t)
+        result.add(t)
         i += 1
         t = get_triangle(i)
 
@@ -52,7 +45,6 @@ triangles = get_triange_numbers(192)
 count = 0
 for w in words:
     s = get_word_score(w)
-    index = binary_search(triangles, s)
-    if index >= 0:
+    if s in triangles:
         count += 1
 print(count)
