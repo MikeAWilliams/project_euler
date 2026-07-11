3c7a7ebcc5c3127b483cdd0b35ab6821e8e1e76c
Mike Williams <mumakin@gmail.com>
2026-07-11 17:09:50 -0600

    p46

--- python/p46.py
+++ python/p46.py
@@ -1,9 +1,25 @@
 from euler_lib import SieveOfEratosthenes
 
-sieve = SieveOfEratosthenes(1000)
-primes = sieve.get_primes_in_sieve()
-print(primes)
+max_prime = 10000
 
-for candidate in range(9, 1000, 2):
+def is_valid(candidate, primes):
+    for p in primes:
+        if p >= candidate:
+            return False
+        for to_square in range(1, 1000):
+            test_val = p + 2 * to_square*to_square
+            if test_val == candidate:
+                return True
+    return False
+
+sieve = SieveOfEratosthenes(max_prime)
+primes = sieve.get_primes_in_sieve()
+
+for candidate in range(9, max_prime, 2):
    if sieve.is_prime(candidate):
       continue
+   if not is_valid(candidate, primes):
+       print(candidate)
+       exit(0)
+print("failed")
+exit(1)
