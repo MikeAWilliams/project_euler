ac742267648c7cfbeeb2559f984283c1c6ddfb1b
Mike Williams <mumakin@gmail.com>
2026-07-11 11:28:40 -0600

    working recursive

--- python/p31.py
+++ python/p31.py
@@ -1,27 +1,20 @@
-def get_value_of_tuple(tuple):
-    return tuple[0]*tuple[1]
-
 coins = [ 200, 100, 50, 20, 10, 5, 2, 1 ]
 
-# plan to place tupls of coin value and count
-results = []
-
-while len(coins) > 0:
-    top_coin = coins[0]
-    coins = coins[1:]
-    starter_list = [(top_coin,1)]
-    multiple = 2
-    while multiple * top_coin <= 200:
-        starter_list.append((top_coin, multiple))
+def get_valid_counts_of_coin(remainder, index):
+    result = []
+    if index >= len(coins):
+        return result
+    multiple = 1
+    while multiple*coins[index] <= remainder:
+        result.append(multiple)
         multiple += 1
-    for starter in starter_list:
-        current_value = get_value_of_tuple(starter)
-        if current_value == 200:
-            results.append(starter)
-            continue
-        this_solution = [starter]
-        for coin_index in range(len(coins)):
+    return result
 
+def recursive_ways(amount, index):
+    total = 0
+    for count in get_valid_counts_of_coin(amount, index):
+        total += recursive_ways(amount - count*coins[index], index + 1)
+    return total
 
-print("results", results)
-print("answer for Euler", len(results))
+print(get_valid_counts_of_coin(200, 0))
+print(recursive_ways(200, 0))
