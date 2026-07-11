d9fc16f5532b2d34b66960d0632699f5fdf4a434
Mike Williams <mumakin@gmail.com>
2026-07-11 11:35:17 -0600

    comments and variable names

--- python/p31.py
+++ python/p31.py
@@ -4,18 +4,19 @@ def get_valid_counts_of_coin(remainder, index):
     result = []
     if index >= len(coins):
         return result
+    # need to include the zero here so that the recursion can contine with zero of this coin
     multiple = 0
     while multiple*coins[index] <= remainder:
         result.append(multiple)
         multiple += 1
     return result
 
-def recursive_ways(amount, index):
-    if amount == 0:
+def recursive_ways(remainder, index):
+    # this iteration hit the total already
+    if remainder == 0:
         return 1
     total = 0
-    for count in get_valid_counts_of_coin(amount, index):
-        total += recursive_ways(amount - count*coins[index], index + 1)
+    for count in get_valid_counts_of_coin(remainder, index):
+        total += recursive_ways(remainder - count*coins[index], index + 1)
     return total
-
 print(recursive_ways(200, 0))
