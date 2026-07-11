c2e716ad29915e4c04f1ae98782694a6d728e38b
Mike Williams <mumakin@gmail.com>
2026-07-11 11:56:47 -0600

    34 is wrong

---
+++ python/p34.py
@@ -0,0 +1,27 @@
+from math import factorial
+
+from euler_lib import get_digits
+
+
+def precompute_digit_factorial():
+    result = {}
+    for i in range(10):
+        result[i] = factorial(i)
+    return result
+
+def test_number(number, factorial_cache):
+    digits = get_digits(number)
+    if len(digits) <= 1:
+        return False
+    result = 0
+    for d in digits:
+        result += factorial_cache[d]
+    return result == number
+
+count = 0
+factorial_cache = precompute_digit_factorial()
+for number in range(100000000):
+    if test_number(number, factorial_cache):
+        print(number)
+        count += 1
+print(count)
