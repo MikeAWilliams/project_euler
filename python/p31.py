0b338fd6f56e633b87628d334fcfc7673d33a2a3
Mike Williams <mumakin@gmail.com>
2026-07-11 11:31:58 -0600

    recursive works

--- python/p31.py
+++ python/p31.py
@@ -4,17 +4,18 @@ def get_valid_counts_of_coin(remainder, index):
     result = []
     if index >= len(coins):
         return result
-    multiple = 1
+    multiple = 0
     while multiple*coins[index] <= remainder:
         result.append(multiple)
         multiple += 1
     return result
 
 def recursive_ways(amount, index):
+    if amount == 0:
+        return 1
     total = 0
     for count in get_valid_counts_of_coin(amount, index):
         total += recursive_ways(amount - count*coins[index], index + 1)
     return total
 
-print(get_valid_counts_of_coin(200, 0))
 print(recursive_ways(200, 0))
