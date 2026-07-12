e89956ec3162bcde6f349a7b3abdcb0e3f8b529d
Mike Williams <mumakin@gmail.com>
2026-07-11 20:53:49 -0600

    it works

--- python/p51.py
+++ python/p51.py
@@ -8,18 +8,30 @@ print("computing sieve")
 sieve = SieveOfEratosthenes(size)
 primes = sieve.get_primes_in_sieve()
 
-print("calculating")
+print("calculating families")
+families = []
 for p in primes:
     digits = get_digits(p)
     digits.reverse()
-    family = [p]
     for number_to_replace in range(1, len(digits)):
-        replace_indexes = itertools.combinations(range(len(digits)), number_to_replace)
-        for new_value in range(10):
-            mutate_digits = digits.copy()
-            for a_replace_set in replace_indexes:
-                for a_replace in a_replace_set:
-                    mutate_digits[a_replace] = new_value
-            new_num = from_digits(mutate_digits)
-            if sieve.is_prime(new_num):
-                family.append(new_num)
+        for replace_set in itertools.combinations(range(len(digits)), number_to_replace):
+            family = []
+            for new_value in range(10):
+                mutate_digits = digits.copy()
+                for index_to_replace in replace_set:
+                    mutate_digits[index_to_replace] = new_value
+                if mutate_digits[0] == 0:
+                    continue
+                new_num = from_digits(mutate_digits)
+                if sieve.is_prime(new_num):
+                    family.append(new_num)
+            if len(family) == 8:
+                families.append(family)
+
+print("smallest member")
+smallest = 1000000000000000
+for f in families:
+    for v in f:
+        if v < smallest:
+            smallest = v
+print(smallest)
