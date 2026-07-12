d9b5dfc43b7b9e707cb677aae8fcb0f091e159bb
Mike Williams <mumakin@gmail.com>
2026-07-11 20:57:12 -0600

    comments

--- python/p51.py
+++ python/p51.py
@@ -13,13 +13,18 @@ families = []
 for p in primes:
     digits = get_digits(p)
     digits.reverse()
+    # replace between 1 and all the digits
     for number_to_replace in range(1, len(digits)):
+        # compute all the indicies to replace for this number of replacements
         for replace_set in itertools.combinations(range(len(digits)), number_to_replace):
+            # a family is all the primes after these digits are replaced with any values 0-9
             family = []
             for new_value in range(10):
                 mutate_digits = digits.copy()
+                # replace all the values at each replace index in the mutate_digits with new_value
                 for index_to_replace in replace_set:
                     mutate_digits[index_to_replace] = new_value
+                # leading zeros make the number smaller and invalid for the family
                 if mutate_digits[0] == 0:
                     continue
                 new_num = from_digits(mutate_digits)
