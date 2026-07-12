d82aff060152c2ae1f07059a85aab2388496acdc
Mike Williams <mumakin@gmail.com>
2026-07-11 20:32:49 -0600

    working on 51

--- python/p51.py
+++ python/p51.py
@@ -1,6 +1,6 @@
-from euler_lib import SieveOfEratosthenes, get_digits
+from euler_lib import SieveOfEratosthenes, from_digits, get_digits
 
-size = 1000000000
+size = 1000000
 
 print("computing sieve")
 sieve = SieveOfEratosthenes(size)
@@ -9,6 +9,11 @@ primes = sieve.get_primes_in_sieve()
 print("calculating")
 for p in primes:
     digits = get_digits(p)
-    if len(digits) < 9:
-        continue
-    print(p)
+    digits.reverse()
+    family = [p]
+    for number_to_replace in range(len(digits)):
+        replace_indexes = itertools.combinations(range(len(digits)), number_to_replace)
+        print(replace_indexes)
+        exit(1)
+        for new_value in range(10):
+            for replace_index in range(len(digits)):
