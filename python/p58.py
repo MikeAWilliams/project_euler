9d97674eff5db53a4d0b933dd8db59c6b595fd2f
Mike Williams <mumakin@gmail.com>
2026-07-25 18:20:02 -0600

    p58 works

--- python/p58.py
+++ python/p58.py
@@ -1,72 +1,30 @@
 from euler_lib import SieveOfEratosthenes
 
-
-def get_grid(size):
-    return [[0 for _ in range(size)] for _ in range(size)]
-
-
-def populate_grid(grid):
-    i = len(grid) - 1
-    j = len(grid) - 1
-    side = len(grid)
-    v = len(grid) * len(grid)
-    dir = "l"
-    steps = side - 1
-    ring_steps = steps
-    while v > 0:
-        grid[i][j] = v
-        v -= 1
-        if dir == "l":
-            if steps > 0:
-                i -= 1
-                steps -= 1
-            else:
-                dir = "u"
-                steps = ring_steps
-        if dir == "u":
-            if steps > 0:
-                j -= 1
-                steps -= 1
-            else:
-                dir = "r"
-                steps = ring_steps
-        if dir == "r":
-            if steps > 0:
-                i += 1
-                steps -= 1
-            else:
-                dir = "d"
-                steps = ring_steps
-        if dir == "d":
-            if steps > 1:
-                j += 1
-                steps -= 1
-            else:
-                dir = "l"
-                ring_steps -= 2
-                steps = ring_steps
-                i -= 1
-
-
 print("computing the sieve")
-grid_size = 10001
+grid_size = 30001
 sieve = SieveOfEratosthenes(grid_size * grid_size)
-print("solving")
-grid = get_grid(grid_size)
-populate_grid(grid)
-for size in range(9, grid_size, 2):
-    origin = (grid_size - size) // 2
-    prime_count = 0
-    diagonal_count = 0
-    for i in range(size):
-        diagonal_count += 2
-        if sieve.is_prime(grid[origin + i][origin + i]):
-            prime_count += 1
-        if sieve.is_prime(grid[origin + i][origin + size - i - 1]):
-            prime_count += 1
-    # above counts the center square twice but since 1 is not prime it won't count primes twice
-    diagonal_count -= 1
-    fraction = prime_count / diagonal_count
-    if fraction < 0.1:
+print("calculating")
+# unfortunately building the actual grid is to memory expensive.
+# Instead we will compute the new corner values at each size step
+
+prime_count = 0
+diagonal_count = 1
+for size in range(3, grid_size, 2):
+    size_minus_1 = size - 1
+    c1 = size * size
+    c2 = c1 - size_minus_1
+    c3 = c1 - 2 * size_minus_1
+    c4 = c1 - 3 * size_minus_1
+    diagonal_count += 4
+    if sieve.is_prime(c1):
+        prime_count += 1
+    if sieve.is_prime(c2):
+        prime_count += 1
+    if sieve.is_prime(c3):
+        prime_count += 1
+    if sieve.is_prime(c4):
+        prime_count += 1
+    ratio = prime_count / diagonal_count
+    if ratio < 0.1:
         print(size)
         break
