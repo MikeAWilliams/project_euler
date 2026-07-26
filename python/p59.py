0fb581f61045866e54f09b266a44b7a6b5c49aa8
Mike Williams <mumakin@gmail.com>
2026-07-25 19:11:05 -0600

    pretty now

--- python/p59.py
+++ python/p59.py
@@ -18,22 +18,34 @@ def decrypt(key, cipher_message):
     return decoded_message
 
 
+def search_for_the_and_for():
+    for l1 in string.ascii_lowercase:
+        for l2 in string.ascii_lowercase:
+            for l3 in string.ascii_lowercase:
+                key = l1 + l2 + l3
+                decoded = decrypt(key, coded_ints)
+                if "the" in decoded and "for" in decoded:
+                    print()
+                    print(key, decoded)
+                    print()
+
+
 coded_ints = read_file()
+
+# search for spaces
+best_key = ""
+best_count = 0
 for l1 in string.ascii_lowercase:
     for l2 in string.ascii_lowercase:
         for l3 in string.ascii_lowercase:
             key = l1 + l2 + l3
             decoded = decrypt(key, coded_ints)
-            if "the" in decoded and "for" in decoded:
-                print()
-                print(key, decoded)
-                print()
-
-print()
-print()
-print()
-print("using one of the above")
-key = "exp"
-decoded = decrypt(key, coded_ints)
+            spaces = decoded.count(" ")
+            if spaces > best_count:
+                best_key = key
+                best_count = spaces
+print("best key", best_key)
+decoded = decrypt(best_key, coded_ints)
+print("message\n", decoded, "\n")
 ascii = [ord(c) for c in decoded]
-print(sum(ascii))
+print("answser", sum(ascii))
